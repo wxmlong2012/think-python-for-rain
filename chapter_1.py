c12ea43266b0d180ce8de651d826eeb1bfb625d3
wxmlong2012 <44708161+wxmlong2012@users.noreply.github.com>
2024-10-09 21:22:28 -0400

    Create chapter_1.py

---
+++ chapter_1.py
@@ -0,0 +1,17 @@
+
+###  try out the following with python code:
+
+
+# 1. In a print statement, what happens if you leave out one of the parentheses, or both?
+
+
+# 2. If you are trying to print a string, what happens if you leave out one of the quotation marks, or both?
+
+
+# 3. You can use a minus sign to make a negative number like -2. What happens if you put a plus sign before a number? What about 2++2?
+
+
+# 4. In math notation, leading zeros are ok, as in 09. What happens if you try this in Python? What about 011?
+
+
+# 5. What happens if you have two values with no operator between them?
